9c86a830a9d262b1fbdd4150f7adc91cc02d88bd
Andrew Glenn <glennan@oregonstate.edu>
2025-07-12 15:04:24 -0700

    Add clarifying comments to models and makes  request test files

--- spec/requests/makes_spec.rb
+++ spec/requests/makes_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Makes", type: :request do
   describe "POST /makes" do
+
+    # Scenario: Creating a Successful Make
+    #   Given I am creating a new Make
+    #   When I fill in "BMW" in the Name field
+    #   And I click "Submit"
+    #   Then a new Make record should be created with a name "BMW"
     context "with valid params" do
       it "creates a new Make and redirects" do
         expect {
@@ -13,6 +19,11 @@ RSpec.describe "Makes", type: :request do
       end
     end
 
+    # Scenario: Creating an Unsuccessful Make
+    #   Given I am creating a new Make
+    #   When I click "Submit"
+    #   Then I should see an error message stating "Name can’t be blank"
+    #   Then a new Make record should not be created
     context "with invalid params" do
       it "does not create a new Make and re-renders the form with errors" do
         expect {
