c4bbec2d56e7fa73aaf2abd9bc3ac3c1a4d1d66a
Andrew Glenn <glennan@oregonstate.edu>
2025-07-12 17:26:54 -0700

    Add cascading dropdown request test for Model /Year dropdowns

--- spec/requests/years_spec.rb
+++ spec/requests/years_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Years", type: :request do
   let(:bmw) { Make.create!(name: "BMW") }
-  let(:series_3) { Model.create!(name: "3 Series", make: bmw) }
+  let!(:audi) { Make.create!(name: "Audi") }
+  let!(:series_3) { Model.create!(name: "3 Series", make: bmw) }
+  let!(:s3) { Model.create!(name: "S3", make: audi) }
+  let!(:year_2022) { Year.create!(year: 2022, model: series_3, make: bmw) }
+  let!(:year_2019) { Year.create!(year: 2019, model: s3, make: audi) }
 
   #  Scenario: Creating a Successful Year
   #   Given I am creating a new Year
@@ -27,7 +31,6 @@ RSpec.describe "Years", type: :request do
       end
     end
 
-
   # Scenario: Creating an Unsuccessful Year
   #   Given I am creating a new Year
   #   Given Make "BMW" exists
@@ -46,6 +49,29 @@ RSpec.describe "Years", type: :request do
         # Should show a validation error for missing model
         expect(response.body).to match(/Model can('|&#39;)t be blank/)
       end
+    end
+  end
+
+    describe "GET /years/for_model" do
+    # Scenario: Car records reference only valid combinations - Model/Year
+    #   Given I am creating a new Car
+    #   Given Make "BMW" exists
+    #   Given Make "Audi" exists
+    #   Given Model "3 Series" exists for "BMW" Make
+    #   Given Model "S3" exists for "Audi" Make
+    #   Given Year "2022" exists for "3 Series" Model and "BMW" Make
+    #   Given Year "2019" exists for "S3" Model and "Audi" Make
+    #   When I select "BMW" from Make
+    #   And I selected "3 Series" from Model
+    #   Then I should see "2022" Year options
+    #   And I should not see "2019" Year options
+    it "returns only years for the selected model/make combination" do 
+      get for_model_years_path, params: { make_id: bmw.id, model_id: series_3.id }
+      expect(response).to have_http_status(:success)
+      json = JSON.parse(response.body)
+      years = json.map { |year| year["year"]}
+      expect(years).to include(2022)
+      expect(years).not_to include(2019)
     end
   end
 end
